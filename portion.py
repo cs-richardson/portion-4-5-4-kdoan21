e7617b5995d5b65f7cca27fe9145be0a5c6db1e7
Kha Doan <kdoan21@ssis.edu.vn>
2019-08-30 11:18:54 +0700

    Create portion.py

---
+++ portion.py
@@ -0,0 +1,16 @@
+# Correct the portion code using rounded floating point nums
+
+# Amount of food and number of people
+tons_of_food = 0.07
+num_people = 25
+
+# Determine how much food each person gets
+tons_of_food_per_person = tons_of_food / num_people
+print("you require ", tons_of_food_per_person, "tons of food per person.")
+
+# Ask the user how much food they took
+tons_taken = float(input("How many tons of food did you take? "))
+if round(tons_taken, 5)  == round(tons_of_food_per_person, 5):
+    print("Good job, you took the right amount of food!")
+else:
+    print("You took the wrong amount of food!")
